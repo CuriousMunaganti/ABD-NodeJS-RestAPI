aa471f978295deebf888855cb6ebe02042e3ddfa
Venkat Pavan Munaganti <munaganti.v@northeastern.edu>
2023-10-06 20:55:50 -0400

    Added JSON schema validations

--- src/api/middleware/validators.js
+++ src/api/middleware/validators.js
@@ -1,10 +1,21 @@
 const logger = require("../../logger/logger")
+const schema = require("../../schemas/plan.schema.json")
+const validate = require('jsonschema').validate
 
 const validatePlan = async (req, res, next) => {
-    logger.info("In payload validator function")
-    next();
+    logger.info("Validating the incoming JSON payload")
+    var result = validate(req.body, schema)
+    if(result.errors.length > 0){
+        const formattedErrors = result.errors.map((error) => ({
+            errorMessage: error.message,
+            errorLocation: error.schema,
+        }));
+        logger.error("Incoming JSONSchema validation failed", formattedErrors)
+        res.status(400).json(formattedErrors)
+        return
+    }
+    next()
 }
-
 
 module.exports ={
     validatePlan: validatePlan
