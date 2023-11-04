46f00f156308349ae98ce57ecddccebde35f5888
Venkat Pavan Munaganti <munaganti.v@northeastern.edu>
2023-11-03 21:29:06 -0400

    Added put request in plan controller

--- src/api/controllers/plan.controller.js
+++ src/api/controllers/plan.controller.js
@@ -1,64 +1,92 @@
 const redis = require('../../services/redis')
 const logger = require('../../logger/logger')
+const etag = require('etag')
 
 const getPlan = async (req, res) => {
     logger.info(`Handling ${req.method} for ${req.originalUrl}`)
-    const key=req.params.id
-    try{
+    const key = req.params.id
+    try {
         const data = await redis.getPlan(key)
-        if(data){
+        if (data) {
             res.status(200).json(data)
-        }else{
-            res.status(404).json({"errorMessage": "Plan not found"})
+        } else {
+            res.status(404).json({ "errorMessage": "Plan not found" })
         }
-    }catch(error){
-        res.status(503).json({"errorMessage": "Serive currently unavailable"})
+    } catch (error) {
+        res.status(503).json({ "errorMessage": "Serive currently unavailable" })
     }
 }
 
 const setPlan = async (req, res) => {
     logger.info(`Handling ${req.method} for ${req.originalUrl}`)
     logger.info(`Payload is, `, req.body)
-    try{
+    try {
         const data = await redis.setPlan(req.body.objectId, req.body)
-        if(data === "OK"){
+        if (data === "OK") {
             res.status(201).json(req.body)
         }
-    }catch(error){
-        res.status(503).json({"errorMessage": "Serive currently unavailable"})
+    } catch (error) {
+        res.status(503).json({ "errorMessage": "Serive currently unavailable" })
     }
 }
 
 const deletPlan = async (req, res) => {
     logger.info(`Handling ${req.method} for ${req.originalUrl}`)
-    const key=req.params.id
-    try{
+    const key = req.params.id
+    try {
         const data = await redis.deletePlan(key)
-        if(data){
+        if (data) {
             res.status(204).json(data)
-        }else{
-            res.status(404).json({"errorMessage": "Plan not found"})
+        } else {
+            res.status(404).json({ "errorMessage": "Plan not found" })
         }
-    }catch(error){
-        res.status(503).json({"errorMessage": "Serive currently unavailable"})
+    } catch (error) {
+        res.status(503).json({ "errorMessage": "Serive currently unavailable" })
     }
 }
 
 const getAllPlan = async (req, res) => {
     logger.info(`Handling ${req.method} for ${req.originalUrl}`)
-    try{
+    try {
         const data = await redis.getAllPlans()
         res.status(200).json({
             message: data
         })
-    }catch(error){
+    } catch (error) {
         res.status(503)
     }
 }
 
+const updatePlan = async (req, res) => {
+    const key = req.params.id
+    const receivedEtag = req.headers['if-match'];
+    const newEtag = etag(JSON.stringify(req.body))
+    if(receivedEtag === newEtag){
+        res.setHeader('ETag', newEtag)
+        res.status(304).send()
+        return
+    }
+
+    try {
+        const existingPlan = await redis.getPlan(key)
+        if (existingPlan) {
+            const data = await redis.setPlan(req.body.objectId, req.body)
+            if (data === "OK") {
+                res.setHeader('ETag', etag(JSON.stringify(req.body)))
+                res.status(200).json(req.body)
+            }
+        } else {
+            res.status(404).json({ "errorMessage": "Plan not found" })
+        }
+    } catch (error) {
+        res.status(503).json({ "errorMessage": "Serive currently unavailable" })
+    }
+}
+
 module.exports = {
-    getAllPlan:getAllPlan,
+    getAllPlan: getAllPlan,
     getPlan: getPlan,
     setPlan: setPlan,
-    deletPlan: deletPlan
+    deletPlan: deletPlan,
+    updatePlan: updatePlan
 };
--- src/api/routes/plan.routes.js
+++ src/api/routes/plan.routes.js
@@ -6,6 +6,7 @@ const plansRouter = express.Router();
 plansRouter.get('/:id', plansController.getPlan);
 plansRouter.post('/', validatePlan, plansController.setPlan);
 plansRouter.delete('/:id', plansController.deletPlan);
+plansRouter.put('/:id', validatePlan, plansController.updatePlan);
 
 module.exports = {
     plansRouter
