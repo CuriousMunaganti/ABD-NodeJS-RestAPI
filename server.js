3b2a617d1fd2a94c494b21c68922120b583ec2e0
Venkat Pavan Munaganti <munaganti.v@northeastern.edu>
2023-12-02 09:49:18 -0500

    updated for demo3

--- server.js
+++ server.js
@@ -2,15 +2,9 @@ require('dotenv').config()
 const app = require('./src/app');
 const express = require('express')
 const logger = require('./src/logger/logger');
-const redisClient = require('./src/services/redis').client;
 app.use(express.json())
 const PORT = process.env.APP_PORT || '8080'
 
 app.listen(PORT, () => {
-    redisClient.connect().then(data => {
-        logger.info("Established connection to the redis server")
-    }).catch(err => {
-        logger.error("Unable to establish connection to the redis server", err)
-    })
     logger.info(`Started the server on the PORT::${PORT}`)
 });
